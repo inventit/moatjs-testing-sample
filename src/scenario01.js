805b2fea3471df3ceb721466540cce61b6bc9679
Toshiyuki Hina <toshiyuki.hina@gmail.com>
2015-06-29 14:44:29 +0900

    refactor

--- src/scenario01.js
+++ src/scenario01.js
@@ -5,41 +5,45 @@ var moat = require('moat'),
 
 var TAG = 'upload-sensing-data';
 
-var sensingData = clientRequest.objects;
-if (!isValidModelObject(sensingData)) {
-  throw 'Invalid model object.';
-}
-
-var resp = session.fetchUrlSync('http://localhost/sensingdata', {
-  method: 'POST',
-  contentType: 'application/json',
-  payload: JSON.stringify(clientRequest.objects)
-});
-
-if (parseInt(resp.responseCode / 100) === 2) {
-  session.log(TAG, 'Success!');
-} else {
-  throw 'Failed to post data: ' + resp.responseCode;
-}
-
-function isValidModelObject(obj) {
-  if (Array.isArray(obj)) {
+function isValidSensingData(sensingData) {
+  if (Array.isArray(sensingData)) {
     session.log(TAG, 'Model object must not be an array.');
     return false;
   }
 
-  if (!obj.hasOwnProperty('temperature')) {
+  if (!sensingData.hasOwnProperty('temperature')) {
     session.log(TAG, '"temperature" field does not exist in model object.');
     return false;
   }
-  if (!obj.hasOwnProperty('humidity')) {
+  if (!sensingData.hasOwnProperty('humidity')) {
     session.log(TAG, '"humidity" field does not exist in model object.');
     return false;
   }
-  if (!obj.hasOwnProperty('timestamp')) {
+  if (!sensingData.hasOwnProperty('timestamp')) {
     session.log(TAG, '"timestamp" field does not exist in model object.');
     return false;
   }
 
   return true;
 }
+
+function main() {
+  var sensingData = clientRequest.objects;
+  if (!isValidSensingData(sensingData)) {
+    throw 'Invalid model object.';
+  }
+
+  var resp = session.fetchUrlSync('http://localhost/sensingdata', {
+    method: 'POST',
+    contentType: 'application/json',
+    payload: JSON.stringify(sensingData)
+  });
+
+  if (parseInt(resp.responseCode / 100) === 2) {
+    session.log(TAG, 'Success!');
+  } else {
+    throw 'Failed to post data: ' + resp.responseCode;
+  }
+}
+
+main();
